533ede03a1c0665aa5a3dbda531d69b44bb85431
mosomomomo <smomma@aicom.co.jp>
2025-06-20 14:55:19 +0900

    kadai_18 コミット

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,22 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		KatoTaro_Chapter18   Taro   = new KatoTaro_Chapter18();
+		KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
+		KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
+		
+		Taro.setGivenName();
+		Taro.commonIntroduce();
+		Taro.eachIntroduce();
+		Ichiro.setGivenName();
+		Ichiro.commonIntroduce();
+		Ichiro.eachIntroduce();
+		Hanako.setGivenName();
+		Hanako.commonIntroduce();
+		Hanako.eachIntroduce();
+
+	}
+
+}
