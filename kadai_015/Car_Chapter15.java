f6dc8e395cf7b1edacff7883dd61a50f236b8951
mosomomomo <smomma@aicom.co.jp>
2025-06-20 11:36:59 +0900

    kadai_15 再コミット

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -6,18 +6,24 @@ public class Car_Chapter15 {
 	private int speed = 10;
 	
 	public void gearChange(int afterGear) {
-		switch(afterGear) {
-		    case 1  -> speed = 10;
-		    case 2  -> speed = 20;
-		    case 3  -> speed = 30;
-		    case 4  -> speed = 40;
-		    case 5  -> speed = 50;
-		    default -> speed = 10;
+		
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+		
+		this.gear = afterGear;
+		
+		switch(this.gear) {
+		    case 1  -> this.speed = 10;
+		    case 2  -> this.speed = 20;
+		    case 3  -> this.speed = 30;
+		    case 4  -> this.speed = 40;
+		    case 5  -> this.speed = 50;
+		    default -> this.speed = 10;
+		    
 		}
 	}
 	
 	public void run() {
-		System.out.println("ギアチェンジ後の速度:" + speed + "km");
+		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 	
 }
